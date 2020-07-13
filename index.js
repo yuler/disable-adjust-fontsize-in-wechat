ee9bb035351a79b881ac694a3338cc1e06b7d951
yuler <joyful4yu@gmail.com>
2020-07-14 01:19:42 +0800

    Fix index.js IIFE wrapper function return

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-;(function(root, factory) {
+;(function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     define(factory)
   } else if (typeof exports === 'object') {
@@ -6,22 +6,26 @@
   } else {
     root.disableAdjustFontSize = factory()
   }
-})(this, function disableAdjustFontSize() {
-  if (typeof WeixinJSBridge === 'object' && typeof WeixinJSBridge.invoke === 'function') {
-    onBridgeReady()
-  } else {
-    if (document.addEventListener) {
-      document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false)
-    } else if (document.attachEvent) {
-      document.attachEvent('WeixinJSBridgeReady', onBridgeReady)
-      document.attachEvent('onWeixinJSBridgeReady', onBridgeReady)
+})(this, function () {
+  function disableAdjustFontSize() {
+    if (typeof WeixinJSBridge === 'object' && typeof WeixinJSBridge.invoke === 'function') {
+      onBridgeReady()
+    } else {
+      if (document.addEventListener) {
+        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false)
+      } else if (document.attachEvent) {
+        document.attachEvent('WeixinJSBridgeReady', onBridgeReady)
+        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady)
+      }
+    }
+
+    function onBridgeReady() {
+      WeixinJSBridge.invoke('setFontSizeCallback', { fontSize: 0 })
+      WeixinJSBridge.on('menu:setfont', function () {
+        WeixinJSBridge.invoke('setFontSizeCallback', { fontSize: 0 })
+      })
     }
   }
 
-  function onBridgeReady() {
-    WeixinJSBridge.invoke('setFontSizeCallback', { fontSize: 0 })
-    WeixinJSBridge.on('menu:setfont', function () {
-      WeixinJSBridge.invoke('setFontSizeCallback', { fontSize: 0 })
-    })
-  }
+  return disableAdjustFontSize
 })
