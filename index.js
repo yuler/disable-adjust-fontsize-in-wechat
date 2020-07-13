24776de2c26ba75a282fb6b38a89eaad2e3cbdf5
yuler <joyful4yu@gmail.com>
2020-07-14 00:24:18 +0800

    Fix index export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export default function disableAdjustFontSize() {
+function disableAdjustFontSize() {
   if (typeof WeixinJSBridge === 'object' && typeof WeixinJSBridge.invoke === 'function') {
     onBridgeReady()
   } else {
@@ -17,3 +17,5 @@ export default function disableAdjustFontSize() {
     })
   }
 }
+
+export default disableAdjustFontSize
